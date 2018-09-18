34a7ff8d2814ba7822367c421e9196822a402fe8
Pawel Baran <pawel.baran@burohappold.com>
2018-09-18 11:20:46 +0200

    DualGraph added

---
+++ Topology_Engine/Create/DualGraph.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using Topologic;
+
+namespace BH.Engine.Topology
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Topologic.DualGraph DualGraph(CellComplex cellComplex, bool useCells, bool useNonManifoldFaces, bool useManifoldFaces, bool useApertures)
+        {
+            return Topologic.DualGraph.ByCellComplex(cellComplex, useCells, useNonManifoldFaces, useManifoldFaces, useApertures);
+        }
+
+        /***************************************************/
+    }
+}
