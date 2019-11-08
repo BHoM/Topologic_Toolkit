9dd96d7fbb61b942e438df78e291a4722ca5e0dc
Al Fisher <al.fisher@burohappold.com>
2019-11-08 18:11:42 +0000

    Updating licence

--- Topologic_Engine/Aperture.cs
+++ Topologic_Engine/Aperture.cs
@@ -6,18 +6,18 @@
  * The project versioning (Git) records all such contribution source information.
  *                                           
  *                                                                              
- * The BHoM is free software: you can redistribute it and/or modify         
- * it under the terms of the GNU Lesser General Public License as published by  
+ * Topologic_Toolkit is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Affero General Public License as published by  
  * the Free Software Foundation, either version 3.0 of the License, or          
  * (at your option) any later version.                                          
  *                                                                              
- * The BHoM is distributed in the hope that it will be useful,              
+ * Topologic_Toolkit is distributed in the hope that it will be useful,              
  * but WITHOUT ANY WARRANTY; without even the implied warranty of               
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
- * GNU Lesser General Public License for more details.                          
+ * GNU Affero General Public License for more details.                          
  *                                                                            
- * You should have received a copy of the GNU Lesser General Public License     
- * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ * You should have received a copy of the GNU Affero General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.      
  */
 
 using System;
